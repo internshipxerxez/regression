163cf12aac729380b3a2d015865d58034a40d8fa
AHAMAD ASHIQ <ahamadashiq95@gmail.com>
2023-10-31 10:19:12 +0530

    log production

--- src/train_and_evaluate_mlops.py
+++ src/train_and_evaluate_mlops.py
@@ -12,6 +12,10 @@ import json
 import numpy as np
 import mlflow
 from urllib.parse import urlparse
+from mlflow.models.signature import infer_signature
+import mlflow.sklearn
+import argparse
+import logging
 
 def eval_metrics(actul, pred):
     rmse = np.sqrt(mean_squared_error(actul,pred))
@@ -22,7 +26,7 @@ def eval_metrics(actul, pred):
 
 
 
-def train_and_evaluate(config_path):
+def train_and_evaluate_mlops(config_path):
     config = read_params(config_path)
     test_data_path = config["split_data"]["test_path"]
     train_data_path = config["split_data"]["train_path"]
@@ -45,37 +49,31 @@ def train_and_evaluate(config_path):
 
     ########################################
 
-    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=random_state)
-    lr.fit(train_x, train_y)
+    mlflow_config = config["mlflow_config"]
+    remote_server_uri = mlflow_config["remote_server_uri"]
 
-    predicted_qualities = lr.predict(test_x)
+    mlflow.set_tracking_uri(remote_server_uri)
+    mlflow.set_experiment(mlflow_config["experiment_name"])
+    with mlflow.start_run(run_name=mlflow_config["run_name"]) as mlops_run:
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=random_state)
+        lr.fit(train_x, train_y)
+        predicted_qualities = lr.predict(test_x)
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
-    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ration", l1_ratio)
 
-    #print("ElasticNet Model (alpha = %f, l1_ratio=%f):" %(alpha, l1_ratio))
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("mae", mae)
+        mlflow.log_metric("r2", r2)
 
-    #print("RMSE:%s" %rmse)
-    #print("MAE:%s" %mae)
-    #print("R2 Score:%s" %r2)
+        tracking_url_type_store = urlparse(mlflow.get_artifact_uri()).scheme
 
-    score_files = config["reports"]["score"]
-    params_files = config["reports"]["params"]
-
-    with open(score_files,"w") as f:
-        scores = {
-            "rmse": rmse,
-            "mae" : mae,
-            "r2" : r2
-        }
-        json.dump(scores, f, indent=4)
-
-    with open(params_files,"w") as f:
-        params = {
-            "alpha": alpha,
-            "l1_ratio" : l1_ratio,
-        }
-        json.dump(params, f, indent=4)
-
+        if tracking_url_type_store !="file" :
+            mlflow.sklearn.log_model(lr, "model", registered_model_name = mlflow_config["registered_model_name"])
+        else:
+            mlflow.sklearn.log_model(lr, "model")  #, signature=signature
+ 
     ################################
 
     os.makedirs(model_dir, exist_ok=True)
@@ -84,9 +82,8 @@ def train_and_evaluate(config_path):
 
 
 
-
 if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args=args.parse_args()
-    train_and_evaluate(config_path=parsed_args.config)
+    train_and_evaluate_mlops(config_path=parsed_args.config)
